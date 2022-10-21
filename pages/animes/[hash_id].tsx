ef33b4643e5389d7adf39ff2d2e21118282d67b8
hulk-higakijin <hm385.chejptks@gmail.com>
2022-10-21 13:12:16 +0900

    style: NavbarとAnimeDetailPageの編集

--- pages/animes/[hash_id].tsx
+++ pages/animes/[hash_id].tsx
@@ -1,36 +1,43 @@
 import { GetServerSideProps, NextPage } from "next";
 import axios from "../../lib/axios";
 import Image from "next/image";
+import { useState } from "react";
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
-  const { hash_id } = context.query
+  const { hash_id } = context.query;
 
-  const res = await axios.get(`/animes/${hash_id}`)
-  const anime = await res.data
+  const res = await axios.get(`/animes/${hash_id}`);
+  const anime = await res.data;
   return {
-    props: {anime}
-  }
-}
+    props: { anime },
+  };
+};
 
-type InitialProps = { anime: Anime }
+type InitialProps = { anime: Anime };
 
 const AnimeDetailPage: NextPage<InitialProps> = ({ anime }) => {
+  const [companyNames] = useState<string>(
+    anime.companies.map((company) => company.name).join(", ")
+  );
+
   return (
     <>
-      <Image src={anime.thumbnail_url} width={500} height={400} />
-      <p>AnimeDetailPage</p>
-      <p>title: {anime.title}</p>
-      <p>title_en: {anime.title_en}</p>
-      <p>title_short1: {anime.title_short1}</p>
-      <p>title_short2: {anime.title_short2}</p>
-      <p>title_short3: {anime.title_short3}</p>
-      <div>companies: 
-        {anime.companies.map((company) => (
-          <p>{company.name }</p>
-        ))}
-      
+      <div className="max-w-6xl mx-auto">
+        <div className="flex flex-col p-2">
+          <h1 className="text-lg font-semibold">
+            {anime.title}（{companyNames}）
+          </h1>
+          <h2 className="text-xs">{anime.title_en}</h2>
+        </div>
+        <div className="flex gap-4">
+          <div className="w-1/3 border rounded">
+            <div className="px-10 py-4">
+              <Image src={anime.thumbnail_url} width={500} height={400} />
+            </div>
+          </div>
+          <div className="w-2/3 bg-blue-500 h-10"></div>
+        </div>
       </div>
-      
     </>
   );
 };

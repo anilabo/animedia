f1555cdf209e82fdc5c4101731e3007cea2726c0
hulk-higakijin <hm385.chejptks@gmail.com>
2022-10-21 14:07:02 +0900

    feat: seach with this season

--- pages/animes/[hash_id].tsx
+++ pages/animes/[hash_id].tsx
@@ -2,6 +2,7 @@ import { GetServerSideProps, NextPage } from "next";
 import axios from "../../lib/axios";
 import Image from "next/image";
 import { useState } from "react";
+import Link from "next/link";
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
   const { hash_id } = context.query;
@@ -17,7 +18,9 @@ type InitialProps = { anime: Anime };
 
 const AnimeDetailPage: NextPage<InitialProps> = ({ anime }) => {
   const [companyNames] = useState<string>(
-    anime.companies.map((company) => company.name).join(", ")
+    anime.companies[0]
+      ? `（${anime.companies.map((company) => company.name).join(", ")}）`
+      : ""
   );
 
   return (
@@ -25,14 +28,21 @@ const AnimeDetailPage: NextPage<InitialProps> = ({ anime }) => {
       <div className="max-w-6xl mx-auto">
         <div className="flex flex-col p-2">
           <h1 className="text-lg font-semibold">
-            {anime.title}（{companyNames}）
+            {anime.title}
+            {companyNames}
           </h1>
           <h2 className="text-xs">{anime.title_en}</h2>
         </div>
         <div className="flex gap-4">
-          <div className="w-1/3 border rounded">
-            <div className="px-10 py-4">
-              <Image src={anime.thumbnail_url} width={500} height={400} />
+          <div className="w-1/3 border rounded px-10 py-4">
+            <Image src={anime.thumbnail_url} width={500} height={400} />
+            <div className="flex">
+              <p className="text-gray-500">
+                {anime.year}, {anime.season}
+              </p>
+              <Link href={`/?q[year_eq]=${anime.year}&q[season_eq]=${anime.season}`}>
+                <a className="ml-auto text-green-600 hover:underline">search with this season</a>
+              </Link>
             </div>
           </div>
           <div className="w-2/3 bg-blue-500 h-10"></div>

23fa057af849a4300d54e11bc95aedaea46f6542
hulk-higakijin <hm385.chejptks@gmail.com>
2022-10-24 20:29:14 +0900

    feat: 季節の名前をnumberに変換するhooksを作成

---
+++ hooks/useSeason.ts
@@ -0,0 +1,11 @@
+const seasons = [
+  { name: "winter", number: 1 },
+  { name: "spring", number: 2 },
+  { name: "summer", number: 3 },
+  { name: "fall", number: 4 },
+];
+
+// convert season name (as string) to season number (as integer)
+export const seasonNum = (seasonName: string) => {
+  return seasons.filter((season) => season.name == seasonName)[0].number;
+};

e1ee06bc09042ba1f2eea05216b56e1e29609b5a
hulk-higakijin <hm385.chejptks@gmail.com>
2022-10-24 20:14:57 +0900

    fix: was foundの文言を不要なので削除

--- components/Anime/FoundList.tsx
+++ components/Anime/FoundList.tsx
@@ -9,13 +9,9 @@ interface InitialProps {
 const AnimeFoundList = ({ animes, keyword }: InitialProps) => {
   return (
     <div className="flex flex-col gap-2">
-      <div className="text-gray-600 flex gap-1 px-2">
-        <p className="text-2xl font-semibold my-auto">{keyword}</p>
-        <p className="mt-auto text-sm">was found some animes.</p>
-      </div>
       <div>
         {animes.map((anime) => (
-          <Link href={`/animes/${anime.public_uid}`}>
+          <Link href={`/animes/${anime.public_uid}`} key={anime.public_uid}>
             <a
               key={anime.public_uid}
               className="grid grid-cols-5 gap-4 hover:bg-green-100 border-b-2 p-2 rounded-t"

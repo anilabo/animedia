013527363b9ae66b2d13a4258f45b896a7aeac22
hulk-higakijin <hm385.chejptks@gmail.com>
2022-10-24 14:54:55 +0900

    style: 検索ページにおいてprogress barを設置

--- pages/search.tsx
+++ pages/search.tsx
@@ -5,6 +5,7 @@ import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
+import ProgressBar from "components/Layouts/ProgressBar";
 
 const SearchPage: NextPage = () => {
   const router = useRouter();
@@ -34,7 +35,7 @@ const SearchPage: NextPage = () => {
           {keyword ? (
             <>
               {isSearching ? (
-                <p>検索中です...</p>
+                <ProgressBar />
               ) : (
                 <>
                   {resultAnimes[0] ? (
@@ -74,9 +75,7 @@ const SearchPage: NextPage = () => {
                     <div className="flex flex-col text-gray-600 px-2 gap-2">
                       <div className="flex gap-2">
                         <p className="text-2xl font-semibold">{keyword}</p>
-                        <p className="mt-auto text-sm">
-                          was not found.
-                        </p>
+                        <p className="mt-auto text-sm">was not found.</p>
                       </div>
                       <p className="text-sm">Please change search keyword.</p>
                     </div>

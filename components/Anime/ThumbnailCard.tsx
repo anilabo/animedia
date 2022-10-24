68d46b3df201f243083ae98354a367cdcbf173af
hulk-higakijin <hm385.chejptks@gmail.com>
2022-10-24 20:16:23 +0900

    fix: search with this seasonのリンクを修正

--- components/Anime/ThumbnailCard.tsx
+++ components/Anime/ThumbnailCard.tsx
@@ -23,7 +23,7 @@ const AnimeThumbnailCard = ({ anime }: InitialProps) => {
         <p className="text-gray-500">
           {anime.year}, {anime.season}
         </p>
-        <Link href={`/?q[year_eq]=${anime.year}&q[season_eq]=${anime.season}`}>
+        <Link href={`/search/?year=${anime.year}&season=${anime.season}`}>
           <a className="ml-auto text-green-600 hover:underline">
             search with this season
           </a>

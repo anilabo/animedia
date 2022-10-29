2e86e4dadafcae07539e1735bf6a12053cfd0545
hulk-higakijin <hm385.chejptks@gmail.com>
2022-10-29 11:49:16 +0900

    fix: 不要なので、Navbarから検索フォームを削除

--- components/Layouts/Navbar.tsx
+++ components/Layouts/Navbar.tsx
@@ -1,21 +1,9 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
-import { FormEvent, useState, useEffect } from "react";
 
 const Navbar = () => {
-  const router = useRouter();
-  const [keyword, setKeyword] = useState<string>("");
-  const searchAnimes = (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    router.push(`/search?keyword=${keyword}`);
-  };
-
-  useEffect(()=> {
-    if (router.isReady) {
-      setKeyword(router.query.keyword as string)
-    }
-  }, [router]);
-
+  const router = useRouter()
+  
   return (
     <>
       <div className="max-w-6xl mx-auto m-2 mb-4">
@@ -23,14 +11,6 @@ const Navbar = () => {
           <Link href="/">
             <a className="text-xl font-bold px-4 py-2">Animedia</a>
           </Link>
-          <form onSubmit={(e) => searchAnimes(e)} className="my-auto">
-            <input
-              className="border border-gray-600"
-              type="text"
-              defaultValue={router.query.keyword}
-              onChange={(e) => setKeyword(e.target.value)}
-            />
-          </form>
         </header>
         <nav className="flex">
           <ul className="grid grid-cols-10 divide-solid w-full text-sm text-center">

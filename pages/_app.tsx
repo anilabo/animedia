d6189187858888dc0d275a7bf0631e194707efb5
hulk-higakijin <hm385.chejptks@gmail.com>
2022-10-30 19:02:58 +0900

    chore: next-authの設定

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,18 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import Navbar from "../components/Layouts/Navbar";
-import NextNProgress from 'nextjs-progressbar';
+import NextNProgress from "nextjs-progressbar";
+import { SessionProvider } from "next-auth/react";
+import { Session } from "next-auth";
 
-function MyApp({ Component, pageProps }: AppProps) {
+function MyApp({ Component, pageProps }: AppProps<{ session: Session }>) {
   return (
     <>
-      <NextNProgress color="#16a34a" />
-      <Navbar />
-      <Component {...pageProps} />
+      <SessionProvider session={pageProps.session}>
+        <NextNProgress color="#16a34a" />
+        <Navbar />
+        <Component {...pageProps} />
+      </SessionProvider>
     </>
   );
 }

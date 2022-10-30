9f8a9945c287ec64ddcda607ed53e9c3fea4f549
hulk-higakijin <hm385.chejptks@gmail.com>
2022-10-30 11:28:03 +0900

    feat: progress barの設置

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,12 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import Navbar from "../components/Layouts/Navbar";
+import NextNProgress from 'nextjs-progressbar';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
+      <NextNProgress color="#16a34a" />
       <Navbar />
       <Component {...pageProps} />
     </>

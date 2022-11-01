527950b5f44bcc00ba03d1310692bcb12df26be2
hulk-higakijin <hm385.chejptks@gmail.com>
2022-11-01 19:46:39 +0900

    refactor: NavbarItemsコンポーネント

--- components/Layouts/Navbar.tsx
+++ components/Layouts/Navbar.tsx
@@ -3,6 +3,7 @@ import Link from "next/link";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import Image from "next/image";
+import NavbarItems from "./NavbarItems";
 
 const Navbar = () => {
   const router = useRouter();
@@ -70,39 +71,7 @@ const Navbar = () => {
             )}
           </div>
         </header>
-        <nav className="flex">
-          <ul className="grid grid-cols-5 md:grid-cols-10 divide-solid w-full text-sm text-center">
-            <Link href="/">
-              <a>
-                <li className="px-2 py-1 border-r border-b-4 border-b-white hover:border-b-green-500">
-                  Home
-                </li>
-              </a>
-            </Link>
-            <li className="px-2 py-1 border-r border-b-4 border-b-white hover:border-b-green-500">
-              Manage
-            </li>
-            <li className="px-2 py-1 border-r border-b-4 border-b-white hover:border-b-green-500">
-              Review
-            </li>
-            <li className="px-2 py-1 border-r border-b-4 border-b-white hover:border-b-green-500">
-              Connect
-            </li>
-            <Link href="/search">
-              <a>
-                <li
-                  className={`px-2 py-1 border-r border-b-4 hover:border-b-green-500 ${
-                    router.pathname.includes("/search")
-                      ? "border-b-green-500"
-                      : "border-b-white"
-                  }`}
-                >
-                  Search
-                </li>
-              </a>
-            </Link>
-          </ul>
-        </nav>
+        <NavbarItems />
       </div>
     </>
   );

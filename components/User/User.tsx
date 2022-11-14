df1ebc78ece98c0985a185f3998c0c18de24eabe
hulk-higakijin <hm385.chejptks@gmail.com>
2022-11-14 14:07:41 +0900

    refactor: UserWatchedCommentコンポーネントの作成

--- components/User/User.tsx
+++ components/User/User.tsx
@@ -1,15 +1,12 @@
 import AnimeList from "components/Anime/List";
-import Comment from "components/Anime/Comment";
-import { useState } from "react";
 import UserSubscribedAt from "./SubscribedAt";
 import UserProfileImage from "./ProfileImage";
 import UserFollowingCount from "./FollowingCount";
+import UserWatchedComments from "./WatchedComments";
 
 type InitialProps = { user: User };
 
 const UserComponent = ({ user }: InitialProps) => {
-  const [watchedUsers, setWatchedUsers] = useState<User[]>([]);
-
   return (
     <div className="max-w-6xl m-2 md:mx-auto mb-10">
       <div className="grid grid-cols-3 gap-4 text-gray-600 ">
@@ -19,19 +16,7 @@ const UserComponent = ({ user }: InitialProps) => {
           <UserSubscribedAt user={user} />
         </div>
         <div className="col-span-2 flex flex-col gap-4">
-          <div className="rounded border">
-            {user.watched_animes.map((anime) => (
-              <Comment
-                anime={anime}
-                user={user}
-                setWatchedUsers={setWatchedUsers}
-                opinion={anime.opinion as string}
-                finishedAt={anime.finished_at as string}
-                visibleAnime={true}
-                key={anime.public_uid}
-              />
-            ))}
-          </div>
+          <UserWatchedComments user={user} />
           <AnimeList animes={user.watched_animes} progress="watched" user={user} />
           <AnimeList animes={user.watching_animes} progress="watching" user={user} />
           <AnimeList animes={user.will_watch_animes} progress="will_watch" user={user} />

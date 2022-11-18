f8fa434c649aaa894b62755128259e6c2d2068a1
hulk-higakijin <hm385.chejptks@gmail.com>
2022-11-18 23:49:52 +0900

    feat: フォロワーカウントの動的化

--- components/User/FollowingCount.tsx
+++ components/User/FollowingCount.tsx
@@ -1,15 +1,31 @@
+import { useEffect, useState } from "react";
+
 interface InitialProps {
-  user: User
+  user: User;
 }
 
 const UserFollowingCount = ({ user }: InitialProps) => {
+  const [followingCount, setFollowingCount] = useState<number>(
+    user.followings.length
+  );
+  const [followerCount, setFollowerCount] = useState<number>(
+    user.followers.length
+  );
+
+  useEffect(() => {
+    setFollowingCount(user.followings.length);
+    setFollowerCount(user.followers.length);
+  }, [user]);
+
   return (
     <div className="rounded border grid grid-cols-2 text-sm divide-x">
       <p className="w-full py-2 m-auto text-center">
-        <b className="font-semibold text-green-500 text-md">9</b> Following
+        <b className="font-semibold text-green-500 text-md">{followingCount}</b>{" "}
+        Following
       </p>
       <p className="w-full py-2 m-auto text-center">
-        <b className="font-semibold text-green-500 text-md">10</b> Followed
+        <b className="font-semibold text-green-500 text-md">{followerCount}</b>{" "}
+        Followed
       </p>
     </div>
   );

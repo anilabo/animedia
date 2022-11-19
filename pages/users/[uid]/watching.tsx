e64db28a72764cf96e785eee5695038f041d3b48
hulk-higakijin <hm385.chejptks@gmail.com>
2022-11-19 10:41:54 +0900

    feat: UserWatchingAllAnimesページの完成

--- pages/users/[uid]/watching.tsx
+++ pages/users/[uid]/watching.tsx
@@ -1,7 +1,31 @@
-const UserWatchingAllAnimesPage = () => {
-  return  (
-    <p>hogehoge</p>
-  )
-}
+import axios from "axios";
+import UserWatchingAllAnimesComponent from "components/User/base/WatchingAllAnime";
+import { isNotFoundCode } from "hooks/useNotFound";
+import { GetServerSideProps, NextPage } from "next";
 
-export default UserWatchingAllAnimesPage
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const { uid } = context.query;
+
+  try {
+    const res = await axios.get(
+      `${process.env.NEXT_PUBLIC_ANILABO_URL}/users/${uid}`
+    );
+    const user = await res.data;
+    return {
+      props: { user },
+    };
+  } catch (error) {
+    return {
+      props: {},
+      notFound: isNotFoundCode(error),
+    };
+  }
+};
+
+type InitialProps = { user: User }
+
+const UserWatchingAllAnimesPage: NextPage<InitialProps> = ({ user }) => {
+  return <UserWatchingAllAnimesComponent user={user} />;
+};
+
+export default UserWatchingAllAnimesPage;

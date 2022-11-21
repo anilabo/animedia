63c4d941efb523ee118661b17ea756e8098bbc29
hulk-higakijin <hm385.chejptks@gmail.com>
2022-11-21 14:04:48 +0900

    refactor: Navbarに関するディレクトリの整理

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
-import Navbar from "../components/Layouts/Navbar";
-import NextNProgress from 'nextjs-progressbar';
+import Navbar from "../components/Layouts/Navbar/Navbar";
+import NextNProgress from "nextjs-progressbar";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (

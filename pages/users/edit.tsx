27f4b537e29892dadd76e1f1437bfb3111d39450
hulk-higakijin <hm385.chejptks@gmail.com>
2022-12-04 23:49:15 +0900

    fix: /editページは使わないので削除

--- pages/users/edit.tsx
+++
@@ -1,8 +0,0 @@
-import UserEditComponent from "components/User/base/Edit";
-import { NextPage } from "next";
-
-const UserEditPage: NextPage = () => {
-  return <UserEditComponent />;
-};
-
-export default UserEditPage;

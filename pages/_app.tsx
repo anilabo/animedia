c0120d374576350808fafe7b06461705d3e79310
hulk-higakijin <hm385.chejptks@gmail.com>
2022-12-05 00:50:10 +0900

    chore: react-toastifyの導入

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,11 +2,14 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import Navbar from "../components/Layouts/Navbar/Navbar";
 import NextNProgress from "nextjs-progressbar";
+import { ToastContainer } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <NextNProgress color="#16a34a" />
+      <ToastContainer />
       <Navbar />
       <Component {...pageProps} />
     </>
